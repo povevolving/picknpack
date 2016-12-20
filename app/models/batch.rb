5e1c2bd5007f9c1e569499e04293fb3fe03123b0
Michael Boyle <mboyle@Michaels-MacBook.local>
2016-12-19 18:38:04 -0800

    Octet stream content type

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -4,8 +4,8 @@ class Batch < ApplicationRecord
   has_attached_file :csv_original
   has_attached_file :csv_processed
 
-  validates_attachment_content_type :csv_original, content_type: ['text/plain', 'text/csv', 'application/vnd.ms-excel']
-  validates_attachment_content_type :csv_processed, content_type: ['text/plain', 'text/csv', 'application/vnd.ms-excel']
+  validates_attachment_content_type :csv_original, content_type: ['text/plain', 'text/csv', 'application/vnd.ms-excel', 'application/octet-stream']
+  validates_attachment_content_type :csv_processed, content_type: ['text/plain', 'text/csv', 'application/vnd.ms-excel', 'application/octet-stream']
 
   after_create :process_original_csv!
 
