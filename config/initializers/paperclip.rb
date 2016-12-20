ce1e1172353597581ce9cb2de9eabf95fcc6181e
Michael Boyle <mboyle@Michaels-MacBook.local>
2016-12-19 18:51:44 -0800

    Adding text/plain to Paperclip content type mappings

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,2 @@
-Paperclip.options[:content_type_mappings] = { csv: 'application/vnd.ms-excel' }
+# Paperclip.options[:content_type_mappings] = { csv: 'application/vnd.ms-excel' }
+Paperclip.options[:content_type_mappings] = {:csv => ["text/comma-separated-values", "text/csv"]}
