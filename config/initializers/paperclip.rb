835d0cddde66b5596d782067b3a48733cd8ed973
Michael Boyle <mboyle@Michaels-MacBook.local>
2016-12-19 19:17:26 -0800

    Hacking paperclip spoofing.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,11 @@
 # Paperclip.options[:content_type_mappings] = { csv: 'application/vnd.ms-excel' }
-Paperclip.options[:content_type_mappings] = {:csv => ["text/comma-separated-values", "text/csv", "application/octet-stream"]}
+# Paperclip.options[:content_type_mappings] = {:csv => ["text/comma-separated-values", "text/csv", "application/octet-stream"]}
+
+require 'paperclip/media_type_spoof_detector'
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end
